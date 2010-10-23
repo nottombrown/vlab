8643bb74d2028be097ff1981eb03127c7955d77a
tombrown <tomfeelslucky@gmail.com>
2010-10-22 18:46:44 -0700

    sketch

---
+++ sketch.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>sketch</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="tombrown">
+	<!-- Date: 2010-10-22 -->
+</head>
+<body>
+
+Look at all this <?php
+echo "sketch shit"
+?>
+
+</body>
+</html>
