838fd378a3776d57de1c1b73bc050e22c658ac47
tombrown <tomfeelslucky@gmail.com>
2010-11-02 17:35:13 -0700

    pages

--- wp-content/themes/mystique-varanasi/patents.php
+++ wp-content/themes/mystique-varanasi/patents.php
@@ -14,29 +14,45 @@
 
 <?php get_header(); ?>
 
-		<div id="container">
-			<div id="content">
-<?php 
-$type = 'patent';
-$args=array(
-  'post_type' => $type,
-  'post_status' => 'publish',
-  'paged' => $paged,
-  'posts_per_page' => 20,
-  'caller_get_posts'=> 1
-);
-$temp = $wp_query;  // assign orginal query to temp variable for later use   
-$wp_query = null;
-$wp_query = new WP_Query($args); 
-?>
+  <!-- main content: primary + sidebar(s) -->
+  <div id="main">
+   <div id="main-inside" class="clear-block">
+    <!-- primary content -->
+    <div id="primary-content">
+     <div class="blocks">
+       <?php do_action('mystique_before_primary'); ?>
+       <?php
+       $type = 'patent';
+       $args=array(
+         'post_type' => $type,
+         'post_status' => 'publish',
+         'paged' => $paged,
+         'posts_per_page' => 20,
+         'caller_get_posts'=> 1
+       );
+       $temp = $wp_query;  // assign orginal query to temp variable for later use   
+       $wp_query = null;
+       $wp_query = new WP_Query($args);
+       
+        if (have_posts()):
+         while (have_posts()):
+          the_post();
+          mystique_page();
+         endwhile;
+        endif;
 
-<?php
+        comments_template();
+       ?>
+       <?php do_action('mystique_after_primary'); ?>
+     </div>
+    </div>
+    <!-- /primary content -->
 
- get_template_part( 'loop', 'index' );?>
-			</div><!-- #content -->
-		    </div><!-- #container -->
+    <?php get_sidebar(); ?>
 
-<?php get_sidebar(); ?>
+   </div>
+  </div>
+  <!-- /main content -->
+
 <?php get_footer(); ?>
-    
-<?php userphoto($posts[0]->post_author); ?>
+
